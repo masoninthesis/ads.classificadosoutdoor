728b37b8e2da23dd5a6e7a1b0aa846b62ff09dfb
Mason L <mason.lawlor@me.com>
2014-10-01 21:54:40 -0700

    fuckt the fuck up

--- sidebar-blog.php
+++
@@ -1,8 +0,0 @@
-<!--Start Sidebar-->
-<div class="sidebar">
-    <?php
-    if (is_active_sidebar('blog-widget-area')) :
-        dynamic_sidebar('blog-widget-area');
-    endif;
-    ?>    
-</div>
